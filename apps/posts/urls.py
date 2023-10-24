09e7ec8664792f6b85cc0349f4c689e9a37ba50e
jsierrabravo <juansierrabravo@outlook.com>
2023-10-23 20:37:41 -0500

    Create the class based view

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from apps.posts import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.post_list, name='index'),
-    path('<int:pk>/', views.post_detail, name='detail'),
+    path('', views.PostList.as_view(), name='index'),
+    path('<int:pk>/', views.PostDetail.as_view(), name='detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
